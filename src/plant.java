e6df6b3e1b4c1d7316d309764d0bd7774ddb9d88
LukasDD82 <lukas.simek.de@gmail.com>
2025-06-04 20:48:29 +0200

    Only Atributs

---
+++ src/plant.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class plant {
+
+    private String name;
+    private String notes;
+    private LocalDate planted;
+    private LocalDate watering;
+    private Integer frequencyOfWatering;
+
+
+
+
+}
