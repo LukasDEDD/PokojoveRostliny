e24413bed010047cea0b3c041ab77a2e0de4db7c
LukasDD82 <lukas.simek.de@gmail.com>
2025-06-05 20:56:03 +0200

    Atributs + Constructor + metods getWateringInfo + doWateringNow

--- src/Plant.java
+++ src/Plant.java
@@ -16,16 +16,64 @@ public class Plant {
         this.frequencyOfWatering = frequencyOfWatering;
     }
 
-    public Plant() {
-        this.notes = "";
-        this.planted = LocalDate.now();
+    public String getWateringInfo() {
+        LocalDate nextWatering = watering.plusDays(frequencyOfWatering);
+        return ("Rostlina" + name + "mela posledni zalivku dne" + watering + "/n " +
+                "je doporucena dalsi zalivka dne : " + nextWatering);
+
+    }
+
+    public void doWateringNow() {
         this.watering = LocalDate.now();
+    }
+
+    public Plant() {
+        this.notes = "";planted = LocalDate.now();watering = LocalDate.now();
 
 }
 
     public Plant( String name) {
-        this.name = name;
-        this.frequencyOfWatering = 7;
+        this.name = name;frequencyOfWatering = 7;
 
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getPlanted() {
+        return planted;
+    }
+
+    public void setPlanted(LocalDate planted) {
+        this.planted = planted;
+    }
+
+    public LocalDate getWatering() {
+        return watering;
+    }
+
+    public void setWatering(LocalDate watering) {
+        this.watering = watering;
+    }
+
+    public Integer getFrequencyOfWatering() {
+        return frequencyOfWatering;
+    }
+
+    public void setFrequencyOfWatering(Integer frequencyOfWatering) {
+        this.frequencyOfWatering = frequencyOfWatering;
     }
 }
