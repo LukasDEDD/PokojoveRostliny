f6b5289101e34d4a1ebcd262596362d414926fe1
LukasDD82 <lukas.simek.de@gmail.com>
2025-06-16 13:16:55 +0200

    metdody pro nacteni a ulozeni souboru s parsovanim + metody pro vypis name + watering a pridani polozky do seznamu txt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.time.LocalDate;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +7,7 @@ public class Main {
         FlowerListManagement manager = new FlowerListManagement();
 
         try {
-            manager.readFromTextFile("resources/plantList.txt", "\t");
+            manager.readFromTextFile("resources/PlantList.txt", "\t");
             for (Plant plant : manager.getAllPlants()) {
                 System.out.println(plant.getWateringInfo());
             }
@@ -16,24 +15,46 @@ public class Main {
             System.err.println("Chyba: " + e.getMessage());
         }
 
-        Plant plant = new Plant ();
+
         try {
 
-            manager.addAllPlants (
-                    List.of(
-                            new Plant("Ruze", "cervena", LocalDate.of(2025, 8, 14), LocalDate.of(2025, 8, 14), 7)
-                    ));
+            manager.addPlant (
+
+                            new Plant("Ruze", "cervena", 7, LocalDate.of(2025, 8, 14), LocalDate.of(2025, 8, 14))
+                    );
 
 
-            manager.writeToTextFile(Settings.getFilePath(),
+            manager.writeToTextFile(Settings.getNewFilePath(),
                     Settings.getDelimiter());
         } catch (PlantException e) {
             System.err.println("Chyba: " + e.getMessage());
             System.err.println("Chyba při zápisu do souboru: "
                     + e.getMessage());   }
 
+        try {
+        for (int i = 1; i < 11; i++) {
+            manager.addPlant(
+                    new Plant("Tulipan",
+                            "Tulipán na prodej " + i,
+                            14,
+                            LocalDate.now(), LocalDate.now()
+                    )
+            );
+        }
 
+        } catch (PlantException e) {
+            System.err.println("Chyba při zápisu do souboru: " + e.getMessage());
+        }
 
-             }
+        try {
+            manager.removePlant(2);
+        }
+        catch (PlantException e) {
+            System.err.println("Chyba při odstraňování rostliny: " + e.getMessage());
+        }
+
 
     }
+        }
+
+
